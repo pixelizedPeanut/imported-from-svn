c86cf879586db60daf81b0094da9e3aa8048e488
Diana Sacuiu <pixelized_peanut@icloud.com>
2015-10-04 10:03:48 +0100

    - start value in html set to 0 - optimising js: - - setting a few global variables for fewer DOM queries and easier to follow code - - removing testing lines and console logs - - add, change, rewrite comments for documentation - - removed old reference code after ensuring all functionalities have been covered - I chose not to implement legacy blink function because operating systems that would use a legacy browser are not supported anymore - I chose to keep the initial comments because they are funny

--- app/js/main.js
+++ app/js/main.js
@@ -6,180 +6,86 @@ var myData = {
 var seconds = 0;
 
 window.addEventListener('load', function(){
+    
     buttons = document.querySelectorAll('.add_amount');
+    calc = document.querySelector('.calc');
+    visible = document.querySelector('#visible');
     
-    // takinng original value and creating output div of total sum
+    // takinng original html value and creating output div for the sum
     var element = document.createElement('div');
-    document.querySelector('.calc').appendChild(element);
+    calc.appendChild(element);
     
-    var startValue = parseInt(document.querySelector('#visible').innerHTML);
+    theSum = document.querySelector('.calc div:last-child');
+      
     // checking for exceptions and setting rules for them
+    var startValue = parseInt(visible.innerHTML);
     if (!startValue)
     {
         startValue = 0;
-        document.querySelector('.calc').style.display = 'none';
+        calc.style.display = 'none'; // hide if 0 or no data to display
     }
     element.textContent = startValue;
-    document.querySelector('#visible').innerHTML = startValue;
-    // unified summing function for all buttons
+    visible.innerHTML = startValue;
+    
+    // main adding function
     for(var i = 0; i < buttons.length; i++) {
         buttons[i].onclick = function() {
             
-            //show data
-            document.querySelector('.calc').style.display = 'block';
+            calc.style.display = 'block'; // show when data
             
-             //disable buttons
+            // disabling buttons and setting wait messages
             for(var i = 0; i < buttons.length; i++) {
                 buttons[i].disabled = true;
             }
-            
-            //add adding text
             if( seconds < 4)
-                document.querySelector('.calc div:last-child').innerHTML = 'adding...';
+                theSum.innerHTML = 'adding...';
             else
-                document.querySelector('.calc div:last-child').innerHTML = 'adding... Get full version to add faster!';
+                theSum.innerHTML = 'adding... Get full version to add faster!';
             
-            //show added amount
+            // show added amount
             var value = this.dataset.amount;
-            document.querySelector('#visible').innerHTML += ' + ' + value;
+            visible.innerHTML += ' + ' + value;
             
-            //add server based delay counter - seconds
+            // add server based delay counter - seconds
+            // the adding and history updating are called inside this function
             delay(seconds++, value, startValue);
-            //console.log('delay', seconds);
-
         }
     }
-    // creating output div for wait message
-        //set rules for wait message 
 });
-        
-function addAmount(amount, startValue) {
-    
-    //add number to visible history
-    addHistory(amount);
-
-    //add number to final sum
-    var sum = startValue;
-    for(var i = 0; i < myData.history.length; i++ ) {
-        sum += parseInt(myData.history[i]);
-    }
-    console.log('sum: ' + sum);    
-    document.querySelector('.calc div:last-child').innerHTML = sum;    
-    //checking for lenght and hiding older values if necessary
-    //if lenght big create link to toggle history
-}
-
-function addHistory(amount) {
-    //save history 
-    //add number to history array
-    i = myData.history.length
-    myData.history[i] = amount;
-    console.log('history', myData.history);
-    //console.log('history', amount);
-}
 
 //delay function
 function delay(seconds, value, startValue) {
 
     var oReq = new XMLHttpRequest();
     
-    oReq.addEventListener("load", function(){ 
-        
-        addAmount(value, startValue);
-        
-        //enable buttons
+    oReq.addEventListener("load", function(){    
+        addAmount(value, startValue); // calling adding   
         for(var i = 0; i < buttons.length; i++) {
-            buttons[i].disabled = false;
-        }
-        
+            buttons[i].disabled = false; // enable buttons
+        }      
     });
     
     oReq.open("GET", "http://www.httpbin.org/delay/" + seconds, true);
-    oReq.send();
-       
+    oReq.send();  
 }
 
-/* commented for reference
+function addAmount(amount, startValue) {
+    
+    addHistory(amount); // calling history
 
-//moved from index from head
+    var sum = startValue;
+    for(var i = 0; i < myData.history.length; i++ ) {
+        sum += parseInt(myData.history[i]);
+    }    
+    theSum.innerHTML = sum;    
+}
+
+function addHistory(amount) {
+    i = myData.history.length 
+    myData.history[i] = amount;
+}
 
 // Before you reuse this script you may want to have your head examined 
 // 
 // Copyright 1999 InsideDHTML.com, LLC.   
 
-function doBlink() { 
-    // Blink, Blink, Blink... 
-    var blink = document.all.tags("BLINK") 
-        for (var i=0; i < blink.length; i++) 
-              blink[i].style.visibility = blink[i].style.visibility == "" ? "hidden" : "" 
-} 
-
-function startBlink() { 
-    // Make sure it is IE4 
-    if (document.all) 
-          setInterval("doBlink()",1000) 
-} 
-window.onload = startBlink; 
-
-
-
-
-
-//originally in this file
-o = {
-  history: [],
-};
-
-element = document.createElement('div');
-setTimeout(function(){
-  document.querySelector('body').insertBefore(element, document.querySelector('button'));
-  element.textContent = 0;
-}, 200)
-
-function addFive () {
-  addhistory(5)
-  element.innerHTML = parseInt(element.innerHTML) + 5;
-}
-function addhistory (Added) {
-document.
-querySelector('span:last-child').appendChild(
-document.
-createElement('span')
-
-)
-document.querySelector('span:last-child').textContent += ' + ' + Added;
-}
-
-
-// onclick="addTen()" --- action from the button with functionality 10
-// moved from index from the bottom
-
-setTimeout(function () {
-document.querySelector('button').onclick = function () {
-  addFive();
-  }}, 200);
-
-// moved from code2.js
-
-function reqListener () {
-   element.textContent = oldNumber + parseInt(10);
-   element2.remove()
-}
-
-function addTen () { 
-  oldNumber = parseInt(element.textContent)
-  addhistory(10)
-element2 = document.createElement('div');
-  element2.textContent = 'adding'
-document.querySelector('body').insertBefore(element2, document.querySelector('button'));
-
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://www.httpbin.org/delay/1", true);oReq.send(); }
-
-element = document.createElement('div');
-setTimeout(function(){
-document.querySelector('body').insertBefore(element, document.querySelector('button'));
-}, 500)
-
-*/
